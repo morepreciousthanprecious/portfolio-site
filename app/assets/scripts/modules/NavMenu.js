b6b1684a4653c4b326ca067874c4a9aee3b7e4c2
Jaclyn <jaclyn.d.touchstone@gmail.com>
2017-08-04 12:26:47 -0500

    moved files to mac

---
+++ app/assets/scripts/modules/NavMenu.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+
+class NavMenu {
+	constructor() {
+		this.menuIcon = $(".nav-bar__menu-icon");
+		this.menuList = $(".nav-bar__list");
+		this.events();
+	}
+	
+	events() {
+		this.menuIcon.click(this.toggleTheMenu.bind(this));
+	}
+
+	toggleTheMenu() {
+		this.menuList.toggleClass("nav-bar__list--is-visible");
+		this.menuIcon.toggleClass("nav-bar__menu-icon--animate");
+	}
+}
+
+export default NavMenu; 
